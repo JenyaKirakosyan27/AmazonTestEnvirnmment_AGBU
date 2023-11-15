a9a945b60a7ed0f3c6e5141ada5e5bc639b6e4e3
JenyaKirakosyan27 <147413285+JenyaKirakosyan27@users.noreply.github.com>
2023-11-16 02:01:21 +0400

    Update cartPage.py

--- pages_/cartPage.py
+++ pages_/cartPage.py
@@ -9,11 +9,11 @@ class CartPage(BasePage):
         self.__firstProductDeleteButtonLocator = (By.XPATH, "(//input[@value = 'Delete'])[1]")
         self.__firstProductSaveForLaterButtonLocator = (By.XPATH, "(//input[@value='Save for later'])[1]")
         self.__allItemsDeselectFromCartLocator = (By.ID, "deselect-all")
-    def delete_firstProduct_from_cart(self):
+    def delete_first_product_from_cart(self):
        firstProductDeleteButtonElement = self._find_element(self.__firstProductDeleteButtonLocator)
        self._click_to_element(firstProductDeleteButtonElement)
 
-    def save_for_later_firstProduct_from_cart(self):
+    def save_for_later_first_product_from_cart(self):
         firstProductSaveForLaterButtonElement = self._find_element(self.__firstProductSaveForLaterButtonLocator)
         self._click_to_element(firstProductSaveForLaterButtonElement)
 
