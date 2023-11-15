4c9475b77623106241e68c9c797cd1553e9757a1
JenyaKirakosyan27 <147413285+JenyaKirakosyan27@users.noreply.github.com>
2023-11-16 01:57:59 +0400

    Update cartPage.py

--- pages_/cartPage.py
+++ pages_/cartPage.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pages_.basePage import BasePage
 
+
 class CartPage(BasePage):
     def __init__(self, driver: webdriver.Chrome):
         super().__init__(driver)
         self.__firstProductDeleteButtonLocator = (By.XPATH, "(//input[@value = 'Delete'])[1]")
         self.__firstProductSaveForLaterButtonLocator = (By.XPATH, "(//input[@value='Save for later'])[1]")
-
+        self.__allItemsDeselectFromCartLocator = (By.ID, "deselect-all")
     def delete_firstProduct_from_cart(self):
        firstProductDeleteButtonElement = self._find_element(self.__firstProductDeleteButtonLocator)
        self._click_to_element(firstProductDeleteButtonElement)
@@ -15,3 +16,6 @@ class CartPage(BasePage):
     def save_for_later_firstProduct_from_cart(self):
         firstProductSaveForLaterButtonElement = self._find_element(self.__firstProductSaveForLaterButtonLocator)
         self._click_to_element(firstProductSaveForLaterButtonElement)
+
+    def deselect_all_items_from_cart(self):
+        allItemsDeselectFromCartElement = self._find_element(self.__allItemsDeselectFromCartLocator)
